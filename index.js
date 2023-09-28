166f1c09abe3520320652c54963598e8bb7d98e8
RKrava <krab9887@gmail.com>
2023-09-28 21:31:03 +0300

    Commit3

--- index.js
+++ index.js
@@ -7,69 +7,78 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.post('/webhook', (req, res) => {
+app.post('/webhook', async (req, res) => {
     console.log('Received Webhook:', req.body);
     console.log('Received Webhook:', req.body?.payment?.products);
     // if (req.body?.paymentsystem === 'custom.fondy') {
     //     req.body.payments = {status: 'paid'}
     // }
+    try {
+        const currentDate = new Date();
+        console.log(1)
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
+        const day = String(currentDate.getDate()).padStart(2, '0');
+        const hours = String(currentDate.getHours() + 3).padStart(2, '0');
+        const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+        const seconds = String(currentDate.getSeconds()).padStart(2, '0');
 
-    const currentDate = new Date();
+        const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
 
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
-    const day = String(currentDate.getDate()).padStart(2, '0');
-    const hours = String(currentDate.getHours()+3).padStart(2, '0');
-    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
-
-    const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
-
-    const dataTotal = {
-        "buyer_comment": req?.body?.comment,
-        "source_id": 3,
-        "ordered_at": formattedDate,
-        "buyer": {
-            "full_name": req?.body?.payment?.delivery_fio,
-            "email": req?.body?.email
-        },
-        "shipping": {
-            "delivery_service_id": 1,
-            "shipping_service": "Нова Пошта",
-            "shipping_receive_point": req?.body?.payment?.delivery_address,
-            "recipient_full_name": req?.body?.payment?.delivery_fio,
-            "recipient_phone": req?.body?.phone,
-        },
-        "products": req.body?.payment?.products.map((item) => {
-            return {
-                "sku": item?.sku,
-                "name": item?.name,
-                "unit_type": "шт",
-                "price": item?.price,
-                "quantity": item?.quantity
-            }
-        }),
-        "payments": [
-            {
-                "payment_method_id": req?.body?.paymentsystem === 'cash' ? 6 : 10,
-                "payment_method": req?.body?.paymentsystem,
-                "amount": req?.body?.payment?.amount,
-                "payment_date": formattedDate,
-                "status": req?.body?.paymentsystem === 'cash' ? "not_paid" : "paid"
-            }
-        ]
-    }
-    axios.post('https://openapi.keycrm.app/v1/order', dataTotal,{
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Cache-Control': 'no-cache',
-            'Pragma': 'no-cache',
-            'Authorization':  'Bearer OWJhMTkyNGFlNDQ0YzQ3NjhiYjU0YzFmYzQxMGVmYmIzMzEwMTBlYQ'
+        console.log(2)
+        const dataTotal = {
+            "buyer_comment": req?.body?.comment,
+            "source_id": 3,
+            "ordered_at": formattedDate,
+            "buyer": {
+                "full_name": req?.body?.payment?.delivery_fio,
+                "email": req?.body?.email
+            },
+            "shipping": {
+                "delivery_service_id": 1,
+                "shipping_service": "Нова Пошта",
+                "shipping_receive_point": req?.body?.payment?.delivery_address,
+                "recipient_full_name": req?.body?.payment?.delivery_fio,
+                "recipient_phone": req?.body?.phone,
+            },
+            "products": req.body?.payment?.products.map((item) => {
+                return {
+                    "sku": item?.sku,
+                    "name": item?.name,
+                    "unit_type": "шт",
+                    "price": item?.price,
+                    "quantity": item?.quantity
+                }
+            }),
+            "payments": [
+                {
+                    "payment_method_id": req?.body?.paymentsystem === 'cash' ? 6 : 10,
+                    "payment_method": req?.body?.paymentsystem,
+                    "amount": req?.body?.payment?.amount,
+                    "payment_date": formattedDate,
+                    "status": req?.body?.paymentsystem === 'cash' ? "not_paid" : "paid"
+                }
+            ]
         }
-    }).then((resp) => {console.log(resp.data); }).catch((error) => {console.log(error)})
 
-    res.status(200).send('OK');
+        console.log(3)
+        const resp = await axios.post('https://openapi.keycrm.app/v1/order', dataTotal, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',
+                'Authorization': 'Bearer OWJhMTkyNGFlNDQ0YzQ3NjhiYjU0YzFmYzQxMGVmYmIzMzEwMTBlYQ'
+            }
+        })
+
+        console.log(resp)
+
+        res.status(200).send('OK');
+    }
+    catch (ex) {
+        console.log("/n Error: ", JSON.stringify(ex))
+    }
 });
 
 const PORT = process.env.PORT || 3000;
